a4b7ca2b73e6f13d15cfdce4751360638115063c
Nathan <nt.bassanetti@gmail.com>
2023-06-14 22:57:11 +0200

    add the link from expertise's projects to Carousel's items

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
      const projects = document.querySelectorAll('.projets > .projet')
      projects.forEach((project, index)  => project.addEventListener('click', () => {
           carousel.goToItem(index)
-     }))
-      })
-
-
+          const carousel_project = document.querySelector(`.carousel-item${index+1}`) as HTMLElement
+          debugger
+          setTimeout(() => {
+               carousel_project.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                    })     
+          },300)
+          }))
+     })
