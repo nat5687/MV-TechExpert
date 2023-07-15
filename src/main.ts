3bc64f20876b40c9036ab00882cc33a44b9b7546
Nathan <nt.bassanetti@gmail.com>
2023-07-15 19:39:48 +0200

    stylise the page especyaly the picture in the backgroud and th header

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {Carousel} from './carousel.ts'
 document.addEventListener('DOMContentLoaded', () => {
      const carousel = new Carousel(document.querySelector('.carousel-wrapper') as HTMLElement)
      const projects = document.querySelectorAll('.projets > .projet')
+     const toTop = document.querySelector('.arrow-top') as HTMLElement
      projects.forEach((project, index)  => project.addEventListener('click', () => {
           carousel.goToItem(index)
           const carousel_project = document.querySelector(`.carousel-item${index+1}`) as HTMLElement
@@ -14,5 +15,18 @@ document.addEventListener('DOMContentLoaded', () => {
                     block: 'start'
                     })     
           },300)
-          }))        
+          })) 
+     const pageHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight)
+     const opacity = Math.max(0.01, (document.documentElement.scrollTop*1.2) / pageHeight)
+     toTop.style.opacity = `${opacity}`
+     window.addEventListener('scroll' ,() => {
+          const opacity = Math.max(0.1, (document.documentElement.scrollTop) / pageHeight)
+          toTop.style.opacity = `${opacity}`
+
+     })
+     toTop?.addEventListener('click', () => {
+          if (document.documentElement.scrollTop > 0) {
+               document.documentElement.scrollIntoView({ behavior: 'smooth' });
+           }
+     })
 })
