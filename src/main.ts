774faa922ccc50864ef0ab89f1cf08daafad9d55
Nathan <nt.bassanetti@gmail.com>
2023-07-23 11:15:25 +0200

    add the footer and many  tiny things

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
      const carousel = new Carousel(document.querySelector('.carousel-wrapper') as HTMLElement)
      const projects = document.querySelectorAll('.projets > .projet')
      const toTop = document.querySelector('.arrow-top') as HTMLElement
+     const imgToTop = document.querySelector('.arrow-top-img') as HTMLElement
      const carousel_container = document.querySelector('.carousel__container') as HTMLElement
      projects.forEach((project, index)  => project.addEventListener('click', () => {
           carousel_container.style.transition = 'transform 0.0s'
@@ -20,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
      // _______________________________TOP ARROWW ___________________________________________________
      const pageHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight)
-     const opacity = Math.max(0.01, (document.documentElement.scrollTop*1.2) / pageHeight)
+     const opacity = (document.documentElement.scrollTop*1.5) / pageHeight
      toTop.style.opacity = `${opacity}`
      window.addEventListener('scroll' ,() => {
           const opacity = Math.max(0.1, (document.documentElement.scrollTop) / pageHeight)
@@ -31,5 +32,19 @@ document.addEventListener('DOMContentLoaded', () => {
           if (document.documentElement.scrollTop > 0) {
                document.documentElement.scrollIntoView({ behavior: 'smooth' });
            }
+     })
+
+     toTop.addEventListener('mouseenter' , () => {
+          console.log('test')
+          imgToTop.classList.add('animate')
+          setTimeout(() => {
+               imgToTop.classList.remove('animate')
+               imgToTop.classList.add('animate-end')
+               
+          }, 500)
+          setTimeout(() => {
+               imgToTop.classList.remove('animate-end')
+               
+          }, 800)
      })
 })
